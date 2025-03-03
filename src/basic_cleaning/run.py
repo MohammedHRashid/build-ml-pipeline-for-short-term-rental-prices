e055ed71cde46563a12050ab45b898e9ac3b7019
MohammedHRashid <humayun.r97@gmail.com>
2025-03-03 14:52:40 +0000

    Update run.py

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -28,6 +28,11 @@ def go(args):
     df = df[idx].copy()
     logging.info('Outliers have been dropped.')
     
+    # Removing rows outside of longitude and latitude range
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+    logging.info('Longitude and Latitude cleaning have been applied')
+    
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
     logging.info("The column last_review has been converted to DateTime format.")
